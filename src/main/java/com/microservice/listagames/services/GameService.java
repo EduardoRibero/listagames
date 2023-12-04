ae12eea907eb4e3f68bc7df3352d4dc810824aff
EduardoRibero <ribeirodomingues2020@gmail.com>
2023-12-04 17:53:42 -0300

    Busca por id lista

--- src/main/java/com/microservice/listagames/services/GameService.java
+++ src/main/java/com/microservice/listagames/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.microservice.listagames.dto.GameDTO;
 import com.microservice.listagames.dto.GameMinDTO;
 import com.microservice.listagames.entities.Game;
+import com.microservice.listagames.projections.GameMinProjection;
 import com.microservice.listagames.repositores.GameRepository;
 
 @Service
@@ -30,4 +31,9 @@ public class GameService {
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
 }
